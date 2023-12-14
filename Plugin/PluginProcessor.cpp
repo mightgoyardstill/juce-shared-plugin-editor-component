9d3f40fbad4a70c8d91705f0d5978c737017916b
mightgoyardstill <63618734+mightgoyardstill@users.noreply.github.com>
2023-12-14 13:37:28 +0000

    Update PluginProcessor.cpp

--- Plugin/PluginProcessor.cpp
+++ Plugin/PluginProcessor.cpp
@@ -2,85 +2,21 @@
 #include "PluginEditor.h"
 
 //==============================================================================
-PluginProcessor::PluginProcessor()
-     : AudioProcessor (BusesProperties()
-                     #if ! JucePlugin_IsMidiEffect
-                      #if ! JucePlugin_IsSynth
-                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
-                      #endif
-                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
-                     #endif
-                       )
+PluginProcessor::PluginProcessor() : AudioProcessor
+(
+    BusesProperties()
+    #if ! JucePlugin_IsMidiEffect
+        #if ! JucePlugin_IsSynth
+        .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+        #endif
+        .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+    #endif
+)
 {
 }
 
 PluginProcessor::~PluginProcessor()
 {
-}
-
-//==============================================================================
-const juce::String PluginProcessor::getName() const
-{
-    return JucePlugin_Name;
-}
-
-bool PluginProcessor::acceptsMidi() const
-{
-   #if JucePlugin_WantsMidiInput
-    return true;
-   #else
-    return false;
-   #endif
-}
-
-bool PluginProcessor::producesMidi() const
-{
-   #if JucePlugin_ProducesMidiOutput
-    return true;
-   #else
-    return false;
-   #endif
-}
-
-bool PluginProcessor::isMidiEffect() const
-{
-   #if JucePlugin_IsMidiEffect
-    return true;
-   #else
-    return false;
-   #endif
-}
-
-double PluginProcessor::getTailLengthSeconds() const
-{
-    return 0.0;
-}
-
-int PluginProcessor::getNumPrograms()
-{
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
-}
-
-int PluginProcessor::getCurrentProgram()
-{
-    return 0;
-}
-
-void PluginProcessor::setCurrentProgram (int index)
-{
-    juce::ignoreUnused (index);
-}
-
-const juce::String PluginProcessor::getProgramName (int index)
-{
-    juce::ignoreUnused (index);
-    return {};
-}
-
-void PluginProcessor::changeProgramName (int index, const juce::String& newName)
-{
-    juce::ignoreUnused (index, newName);
 }
 
 //==============================================================================
@@ -97,57 +33,32 @@ void PluginProcessor::releaseResources()
     // spare memory, etc.
 }
 
-bool PluginProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
-{
-  #if JucePlugin_IsMidiEffect
-    juce::ignoreUnused (layouts);
-    return true;
-  #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    // Some plugin hosts, such as certain GarageBand versions, will only
-    // load plugins that support stereo bus layouts.
-    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
-        return false;
-
-    // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
-    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
-        return false;
-   #endif
-
-    return true;
-  #endif
-}
-
 void PluginProcessor::processBlock (juce::AudioBuffer<float>& buffer,
                                               juce::MidiBuffer& midiMessages)
 {
     juce::ignoreUnused (midiMessages);
 
-    // const auto pos = [&]
-    // {
-    //     if (auto* ph = getPlayHead())
-    //         if (auto result = ph->getPosition())
-    //             return *result;
+    const auto pos = [&]
+    {
+        if (auto* ph = getPlayHead())
+            if (auto result = ph->getPosition())
+                return *result;
 
-    //     DBG ("No playhead available, using default values");
-    //     return juce::AudioPlayHead::PositionInfo{};
-    // }();
+        DBG ("No playhead available, using default values");
+        return juce::AudioPlayHead::PositionInfo{};
+    }();
 
-    // const auto sig = pos.getTimeSignature().orFallback (juce::AudioPlayHead::TimeSignature{});
+    const auto sig = pos.getTimeSignature().orFallback (juce::AudioPlayHead::TimeSignature{});
 
-    // DBG (juce::String (pos.getBpm().orFallback (120.0), 2) << " bpm, "
-    //      << sig.numerator << '/' << sig.denominator
-    //      << " - " << timeToTimecodeString (pos.getTimeInSeconds().orFallback (0.0))
-    //      << " - " << quarterNotePositionToBarsBeatsString (pos.getPpqPosition().orFallback (0.0), sig));
+    DBG (juce::String (pos.getBpm().orFallback (120.0), 2) << " bpm, "
+         << sig.numerator << '/' << sig.denominator
+         << " - " << timeToTimecodeString (pos.getTimeInSeconds().orFallback (0.0))
+         << " - " << quarterNotePositionToBarsBeatsString (pos.getPpqPosition().orFallback (0.0), sig));
 
-    // if (pos.getIsRecording())     DBG ("  (is recording)");
-    // else if (pos.getIsPlaying())  DBG ("  (is playing)");
+    if (pos.getIsRecording())     DBG ("  (is recording)");
+    else if (pos.getIsPlaying())  DBG ("  (is playing)");
 
-
-
+     
     juce::ScopedNoDenormals noDenormals;
     auto totalNumInputChannels = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
@@ -176,17 +87,6 @@ void PluginProcessor::processBlock (juce::AudioBuffer<float>& buffer,
 }
 
 //==============================================================================
-bool PluginProcessor::hasEditor() const
-{
-    return true; // (change this to false if you choose to not supply an editor)
-}
-
-juce::AudioProcessorEditor* PluginProcessor::createEditor()
-{
-    return new PluginEditor (*this);
-}
-
-//==============================================================================
 void PluginProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
     // You should use this method to store your parameters in the memory block.
@@ -200,4 +100,20 @@ void PluginProcessor::setStateInformation (const void* data, int sizeInBytes)
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
     juce::ignoreUnused (data, sizeInBytes);
+}
+
+
+
+//==============================================================================
+// This creates a new instance of the plugin's editor..
+juce::AudioProcessorEditor* PluginProcessor::createEditor()
+{
+    return new PluginEditor (*this);
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new PluginProcessor();
 }
