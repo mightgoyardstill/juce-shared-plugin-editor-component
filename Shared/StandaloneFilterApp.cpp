517458978126435089c32114c16210fa75e7fc30
mightgoyardstill <63618734+mightgoyardstill@users.noreply.github.com>
2023-12-15 00:31:01 +0000

    Delete Shared directory

--- Shared/StandaloneFilterApp.cpp
+++
@@ -1,8 +0,0 @@
-#include "StandaloneFilterApp.h"
-
-extern juce::JUCEApplicationBase* juce_CreateApplication();
-
-juce::JUCEApplicationBase* juce_CreateApplication() 
-{
-    return new StandaloneFilterApp(); 
-}
